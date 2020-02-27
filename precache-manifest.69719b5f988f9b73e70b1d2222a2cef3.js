d1d77b1af4b84d2580adf677d6ecea0ec88e47e9
Khalimudin <khalimudin@gmail.com>
2020-02-28 03:37:53 +0700

    Updates

---
+++ precache-manifest.69719b5f988f9b73e70b1d2222a2cef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48678f499ed1d75ed11a014d389e7f6a",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "5b72cbdcdcff0533a7e7",
+    "url": "/furniture-finder/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "e47bfb56bc83bd727354",
+    "url": "/furniture-finder/static/css/main.02b5d264.chunk.css"
+  },
+  {
+    "revision": "5b72cbdcdcff0533a7e7",
+    "url": "/furniture-finder/static/js/2.b75d894e.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/furniture-finder/static/js/2.b75d894e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47bfb56bc83bd727354",
+    "url": "/furniture-finder/static/js/main.20991789.chunk.js"
+  },
+  {
+    "revision": "053d0ca55247a158a84f",
+    "url": "/furniture-finder/static/js/runtime-main.006d80cd.js"
+  }
+]);
